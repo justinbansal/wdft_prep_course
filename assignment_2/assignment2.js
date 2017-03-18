ed8873e70d8fc1d54a6e1322a4ba8cd9d323c930
Justin Bansal <justinbansal@gmail.com>
2017-03-18 19:54:03 -0400

    completed assignments 1 and 2

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -3,7 +3,9 @@ Exercise #1
 Create a for loop that prints out the numbers 1 to 100 in the console.
 */
 
-
+for (i=1;i<101;i++){
+    console.log(i)
+}
 
 /*
 Exercise #2
@@ -17,3 +19,29 @@ Write a loop that makes seven calls to console.log to output the following trian
 ######
 #######
 */
+
+// My first attempt the day I learned about for loops and arrays
+var signs=["#","##"];
+
+for (i="#";i<signs.length;i++){
+    console.log(signs[i]);
+}
+
+// My second attempt the day after I had learned about while and do while loops
+var triangle = ["#","##","###","####","#####","######","#######"]
+for (var i=0;i<triangle.length;i++) {
+     console.log[i];
+    }
+    
+// This has to be the one!
+
+var triangle = ["#","##","###","####","#####","######","#######"];
+for (var i=0;i<triangle.length;i++) {
+     console.log([i])
+    }
+    
+// Got it!
+var triangle = ["#","##","###","####","#####","######","#######"];
+for (var i=0;i<triangle.length;i++) {
+     console.log(triangle[i])
+     }
